ab83c8517338f39560e0092dade54241ef05c160
Mohamed Bassel <48108775+mohamedbassel24@users.noreply.github.com>
2020-01-26 18:55:54 +0200

    Progress in contact us page
    Added a form and form handler, some validation didn't add yet, some components didn't fit properly.

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
+
+  form: FormGroup;
+  profileForm = new FormGroup({
+   
+    Name: new FormControl('', [Validators.required]),
+    Email: new FormControl('', [Validators.required, Validators.email]),
+    Phone: new FormControl('', [Validators.required]),
+    Message: new FormControl('', [Validators.required]),
+  });
+  onSubmit() {
+  }
+
   constructor() { }
 
   ngOnInit() {
+
+    this.form = new FormGroup({
+      // tslint:disable-next-line: max-line-length
+      Name: new FormControl(null, { validators: [Validators.pattern('[^0-9\.\?\!\@\#\$\%\^\&\*\(\)\<\>\{\}]+')] }),
+      Email: new FormControl(null, { validators: [Validators.required, Validators.email] }),
+      Phone: new FormControl(null, { validators: [Validators.minLength(8)] }), // TODO: ADD validation on numbers 
+      Message: new FormControl(null, { validators: [Validators.required] }),
+ 
+    });
+
+
   }
 
 }
